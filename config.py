3c874cb194fb0a96ddd4730ed031ad5f5fcb86fb
Axenov9 <alex2-7-9@yandex.ru>
2024-04-15 22:42:49 +0300

    gitig

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+from secrets import *
+
 BOARD_ID = '4KehDIuj'
-TG_API = "7030845455:AAG4DpkF3EoUoQtuPhDSkJvvm9Lw-0vVHcU"
-TRELLO_API_KEY = 'c732ebccabd1361c15fd526255713736'
-TRELLO_API_TOKEN = 'ATTA31b0256e6aa77f655b1e2a66b514ba00b3a654f86b810c3337fe91a44358bd6c5CB95D3E'
-actions_api = "https://api.trello.com/1/boards/4KehDIuj/actions?key=c732ebccabd1361c15fd526255713736&token=ATTA31b0256e6aa77f655b1e2a66b514ba00b3a654f86b810c3337fe91a44358bd6c5CB95D3E"
+TG_API = TG_API
+TRELLO_API_KEY = TRELLO_API_KEY
+TRELLO_API_TOKEN = TRELLO_API_TOKEN
 chat_id = -1002076472478 # make sure you change this value, using 0 is not allowed!
 # chat_id = 249562441
 delay = 30
